525b759bdfc6837cee197aa874b7f7dfee8cbbf3
Rahul Kumar <r1303yadav@gmail.com>
2025-08-28 12:45:10 +0530

    feat(user-auth): implement user model, forms, and admin configuration

--- core_apps/user_auth/admin.py
+++ core_apps/user_auth/admin.py
@@ -1,3 +1,89 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
+from .forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    model = User
+    list_display = [
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "role",
+    ]
+    list_filter = ["email", "is_staff", "is_active", "role"]
+
+    fieldsets = (
+        (
+            _("Login Credentials"),
+            {
+                "fields": (
+                    "username",
+                    "emails",
+                    "password",
+                )
+            },
+        ),
+        (
+            _("Personal Info"),
+            {
+                "fields": (
+                    "first_name",
+                    "middle_name",
+                    "last_name",
+                    "id_no",
+                    "role",
+                )
+            },
+        ),
+        (
+            _("Account Status"),
+            {
+                "fields": (
+                    "account_status",
+                    "failed_login_attempts",
+                    "last_failed_login",
+                )
+            },
+        ),
+        (
+            _("Security"),
+            {
+                "fields": (
+                    "security_questions",
+                    "security_answer",
+                )
+            },
+        ),
+        (
+            _("Permissions and Groups"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            _("Important Dates"),
+            {
+                "fields": (
+                    "date_joined",
+                    "last_login",
+                )
+            },
+        ),
+    )
+    search_fields = ["email", "username", "first_name", "last_name"]
+    ordering = ["email"]

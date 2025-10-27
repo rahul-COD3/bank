022fe37178406d25c463b6483b49c7c1f0e0352c
Rahul Kumar <r1303yadav@gmail.com>
2025-10-27 14:02:15 +0530

    feat(user-profile): add profile and next of kin management views, serializers, and URLs

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     ),
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("core_apps.user_auth.urls")),
+    path("api/v1/profiles/", include("core_apps.user_profile.urls")),
 ]
 
 admin.site.site_header = "NextGen Bank Admin"
